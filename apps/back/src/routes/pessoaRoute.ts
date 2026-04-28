084682610f2e12ac98e7d07c04fd86eb32fc1d88
Felipe Anhucci <168142961+fanhucci@users.noreply.github.com>
2026-04-27 22:13:21 -0300

    Update pessoaRoute.ts

--- apps/back/src/routes/pessoaRoute.ts
+++ apps/back/src/routes/pessoaRoute.ts
@@ -5,7 +5,7 @@ import PessoaController from "../controllers/pessoaController";
 const router = Router();
 const ctrl = new PessoaController();
 
-router.get(`/pessoas`, ctrl.listarPessoas.bind(ctrl));
+//router.get(`/pessoas`, ctrl.listarPessoas.bind(ctrl));
 router.post(`/pessoas`, ctrl.adicionarPessoa.bind(ctrl));
 router.patch(`/pessoas/:id`, ctrl.editarPessoa.bind(ctrl));
 router.patch(`/pessoas/:id/status`, ctrl.alterarStatus.bind(ctrl));
