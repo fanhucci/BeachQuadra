7568214ebd1e064e3384958298ab788f3f4db793
fanhucci <felipeanhucci38@gmail.com>
2026-05-01 02:46:51 -0300

    adicionado exportação de agendamentoSchema

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -3,3 +3,4 @@ export * from "./schemas/horarioSchema";
 export * from "./schemas/quadraSchema";
 export * from "./schemas/contaSchema";
 export * from "./schemas/usuarioSchema";
+export * from "./schemas/agendamentoSchema";
