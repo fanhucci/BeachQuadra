fe4cb203d8311bf380f32e66fbdc2666246bc9ce
fanhucci <felipeanhucci38@gmail.com>
2026-05-02 19:52:17 -0300

    teste de frontend para cadastrar reservas

--- apps/front/app/(protected)/reservas/cadastrar/useCadastroReservas.ts
+++ apps/front/app/(protected)/reservas/cadastrar/useCadastroReservas.ts
@@ -81,7 +81,7 @@ export default function useCadastroReservas(){
 
         setHorarioSelecionado((prev)=>({
             ...prev,
-            reservas:
+            //reservas:
         }))
     }
 
