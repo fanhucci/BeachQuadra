5f4693aba4aeb028f70789be6abf71f9d6bf6167
fanhucci <felipeanhucci38@gmail.com>
2026-05-02 02:53:06 -0300

    criação de rota de horarios disponiveis

--- apps/back/src/repositories/horarioRepository.ts
+++ apps/back/src/repositories/horarioRepository.ts
@@ -36,7 +36,7 @@ export default class HorarioRepository {
     async retornarHorariosPermitidos( horarios:Date[]){
         return await sql`
             with lista_horarios as (
-                select unnest(${sql(horarios)}::timestamptz[]) as horario
+                select unnest(${sql(horarios as any, 'timestamptz[]')}) as horario
             )
             select 
                 h.horario, 
