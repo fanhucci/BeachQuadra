74708393bdc6f453d0676ef54c9f50f3b648517b
fanhucci <felipeanhucci38@gmail.com>
2026-05-02 22:10:52 -0300

    teste de seleção de slots de horario

--- apps/front/app/(protected)/reservas/cadastrar/page.tsx
+++ apps/front/app/(protected)/reservas/cadastrar/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Check } from "lucide-react";
+import { ArrowLeft, ArrowRight, Check } from "lucide-react";
 import useCadastroReservas from "./useCadastroReservas";
 
 export default function CadastroReservasPage(){
@@ -18,7 +18,7 @@ export default function CadastroReservasPage(){
                     disabled={pagina === 0}
                     className="px-3 py-1.5 text-sm rounded-md border bg-white hover:bg-gray-100 disabled:opacity-30"
                 >
-                    ← Anterior
+                    <ArrowLeft /> Semana Anterior
                 </button>
 
                 <h2 className="text-base font-semibold text-gray-700">
@@ -30,83 +30,85 @@ export default function CadastroReservasPage(){
                     disabled={(pagina + 1) * 7 >= diasMeses.length}
                     className="px-3 py-1.5 text-sm rounded-md border bg-white hover:bg-gray-100 disabled:opacity-30"
                 >
-                    Próxima →
+                    Próxima Semana <ArrowRight />
                 </button>
             </div>
 
                     
-            <div className="flex-1 flex justify-center px-6 py-4">
-                <div className="w-full max-w-6xl border rounded-xl bg-white shadow-sm p-3 flex flex-col">
+            <div className="flex flex-row">
+                <div className="flex-1 flex justify-center px-6 py-4">
+                    <div className="w-full max-w-6xl border rounded-xl bg-white shadow-sm p-3 flex flex-col">
 
-                    <div
-                        className="grid gap-2 mb-2 text-center"
-                        style={{ gridTemplateColumns: `repeat(${diasVisiveis.length}, 1fr)` }}
-                    >
-                        {diasVisiveis.map((dia, index) => (
-                            <div key={dia} className="flex flex-col">
-                                <span className="text-[10px] uppercase text-gray-400">
-                                    {tableHeaders[index]}
-                                </span>
-                                <span className="text-sm font-semibold text-gray-700">
-                                    {dia}
-                                </span>
-                            </div>
-                        ))}
-                    </div>
+                        <div
+                            className="grid gap-2 mb-2 text-center"
+                            style={{ gridTemplateColumns: `repeat(${diasVisiveis.length}, 1fr)` }}
+                        >
+                            {diasVisiveis.map((dia, index) => (
+                                <div key={dia} className="flex flex-col">
+                                    <span className="text-[10px] uppercase text-gray-400">
+                                        {tableHeaders[index]}
+                                    </span>
+                                    <span className="text-sm font-semibold text-gray-700">
+                                        {dia}
+                                    </span>
+                                </div>
+                            ))}
+                        </div>
 
-                    
-                    <div
-                        className="flex-1 grid gap-2"
-                        style={{
-                            gridTemplateColumns: `repeat(${diasVisiveis.length}, 1fr)`,
-                            gridTemplateRows: `repeat(${horarioSemana.length}, 1fr)`,
-                        }}
-                    >
-                        {horarioSemana.map((hora) =>
-                            diasVisiveis.map((dia) => {
-                            const slot = dados[dia]?.[hora];
-                            const permitido = slot?.permitido;
+                        
+                        <div
+                            className="flex-1 grid gap-2"
+                            style={{
+                                gridTemplateColumns: `repeat(${diasVisiveis.length}, 1fr)`,
+                                gridTemplateRows: `repeat(${horarioSemana.length}, 1fr)`,
+                            }}
+                        >
+                            {horarioSemana.map((hora) =>
+                                diasVisiveis.map((dia) => {
+                                const slot = dados[dia]?.[hora];
+                                const permitido = slot?.permitido;
 
-                            const isSelected = horarioSelecionado.reservas.some(
-                                (r) => r.horario === slot?.horario
-                            );
+                                const isSelected = horarioSelecionado.reservas.some(
+                                    (r) => r.horario === slot?.horario
+                                );
 
-                            return (
-                                <button
-                                key={dia + hora}
-                                type="button"
-                                onClick={() => selecionarHorario(slot)}
-                                disabled={!permitido}
-                                className={`
-                                    w-full h-full rounded-md text-[10px] font-medium
-                                    flex items-center justify-center
-                                    transition border
-                                    ${
-                                    isSelected
-                                        ? "bg-blue-600 text-white border-blue-700"
-                                        : permitido
-                                        ? "bg-green-50 text-green-700 border-green-200 hover:bg-green-100"
-                                        : "bg-gray-100 text-gray-300 border-gray-200 cursor-not-allowed"
-                                    }
-                                `}
-                                >
-                                {isSelected? (<Check size={16}/>) : hora}
-                                </button>
-                            );
-                            })
-                        )}
+                                return (
+                                    <button
+                                    key={dia + hora}
+                                    type="button"
+                                    onClick={() => selecionarHorario(slot)}
+                                    disabled={!permitido}
+                                    className={`
+                                        w-full h-full rounded-md text-[10px] font-medium
+                                        flex items-center justify-center
+                                        transition border
+                                        ${
+                                        isSelected
+                                            ? "bg-blue-600 text-white border-blue-700 cursor-pointer hover:bg-blue-100"
+                                            : permitido
+                                            ? "bg-green-50 text-green-700 border-green-200 cursor-pointer hover:bg-green-100 "
+                                            : "bg-gray-100 text-gray-300 border-gray-200 cursor-not-allowed"
+                                        }
+                                    `}
+                                    >
+                                    {isSelected? (<Check size={16}/>) : hora}
+                                    </button>
+                                );
+                                })
+                            )}
+                        </div>
                     </div>
                 </div>
-            </div>
 
             
-            <div className="px-8 py-4 bg-white border-t flex justify-end shrink-0">
-                <button
-                    onClick={salvarReservas}
-                    className="px-6 h-10 rounded-md bg-blue-600 text-white font-medium hover:bg-blue-700 transition"
-                >
-                    Reservar
-                </button>
+                <div className="px-8 py-4 bg-white border-t flex justify-end shrink-0">
+                    <button
+                        onClick={salvarReservas}
+                        className="px-6 h-10 rounded-md bg-blue-600 text-white font-medium hover:bg-blue-700 transition"
+                    >
+                        Reservar
+                    </button>
+                </div>
             </div>
         </div>
     );
