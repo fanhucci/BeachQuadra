f29b07374a2a0a9eebeb455ed50da432cf3a7b41
fanhucci <felipeanhucci38@gmail.com>
2026-05-03 05:10:28 -0300

    prototipo pagina de detalhes de agendamento

--- apps/front/app/(protected)/agendamento/[id]/page.tsx
+++ apps/front/app/(protected)/agendamento/[id]/page.tsx
@@ -4,10 +4,35 @@ import useDetailAgendamento from "./useDetailAgendamento"
 
 export default function AgendamentoDetailPage(){
     const {agendamento} = useDetailAgendamento();
+    if(!agendamento) return <>Carregando...</>
 
+    const cliente = agendamento.cliente;
+    const responsavel = agendamento.criado_por;
+    const reservas = agendamento.reservas;
+    
     return(
-        <p>
-            {JSON.stringify(agendamento)}
-        </p>
+        <div className="flex flex-1 w-full p-2">
+            <div className="">
+                <div>
+                    {
+                        JSON.stringify(cliente)
+
+                    }
+                </div>
+                <div>
+                    {
+                        JSON.stringify(responsavel)
+
+                    }
+                </div>
+                <div>
+                    {
+                        JSON.stringify(reservas)
+
+                    }
+                </div>
+
+            </div>
+        </div>
     )
 }
