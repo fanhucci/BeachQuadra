9d0cc33a280f568886bff1c3af767b9158c21908
fanhucci <felipeanhucci38@gmail.com>
2026-05-04 05:09:18 -0300

    prototipo pagina de agendamento

--- apps/front/app/(protected)/agendamento/page.tsx
+++ apps/front/app/(protected)/agendamento/page.tsx
@@ -12,7 +12,7 @@ export default function AgendamentosPage(){
     const tabela = agendamentos.map((d)=>({
         nome:d.nome,
         status:d.status,
-        valor_total:d.valor_total.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
+        valor_total:d.valor_total.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }),
         acoes:(
         <CustomButtom funcao={()=>router.replace(`/agendamento/${d.id_agendamento}`)} texto="Detalhes" tipo="secundario"/>)
     }))
