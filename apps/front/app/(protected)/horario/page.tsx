641e9e8397beaa76ad7dc0f0ab021145beb81cf5
fanhucci <felipeanhucci38@gmail.com>
2026-05-08 20:39:15 -0300

    prototipo pagina horarios

--- apps/front/app/(protected)/horario/page.tsx
+++ apps/front/app/(protected)/horario/page.tsx
@@ -47,7 +47,7 @@ export default function HorarioPage(){
                                 type="checkbox"
                                 className="sr-only peer"
                                 checked={h.ativo}
-                                onChange={(e) => handleChange(h.id_horario, 'dia_semana', e.target.checked)}
+                                onChange={(e) => handleChange(h.id_horario, 'ativo', e.target.checked)}
                             />
                             <div className="w-9 h-5 bg-gray-300 peer-focus:outline-none rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-blue-600">   
                             </div>
