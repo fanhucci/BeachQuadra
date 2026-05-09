d51058f88d92e94ed21f24310363887cba8c7832
fanhucci <felipeanhucci38@gmail.com>
2026-05-09 05:04:01 -0300

    criação de rotas de bloqueio

--- apps/back/src/repositories/bloqueioRepository.ts
+++ apps/back/src/repositories/bloqueioRepository.ts
@@ -7,7 +7,7 @@ export default class BloqueioRepository{
             insert into dias_bloqueados
             (motivo,inicio_bloqueio,fim_bloqueio)
             values(${dados.motivo,dados.inicio_bloqueio,dados.fim_bloqueio})
-            returninig id_bloqueio
+            returning id_bloqueio
         `;
     }
 
