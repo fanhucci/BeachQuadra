64c0ecb88de1f98aebfaf75d3e712e4f7d74f46b
fanhucci <felipeanhucci38@gmail.com>
2026-05-13 20:00:21 -0300

    att de quadra controller

--- apps/back/src/routes/agendamentoRoute.ts
+++ apps/back/src/routes/agendamentoRoute.ts
@@ -6,9 +6,9 @@ const router = Router();
 const ctrl = new AgendamentoController();
 const authMiddleware = new AuthMiddleware();
 
-router.get(`/agendamento`,authMiddleware.auth, ctrl.listarAgendamentos.bind(ctrl));
-router.get(`/agendamento/:id`,authMiddleware.auth, ctrl.buscarAgendamentoPorId.bind(ctrl));
-router.post(`/agendamento/:id`, authMiddleware.auth, ctrl.cadastrarNovoAgendamento.bind(ctrl));
-router.patch(`/agendamento/:id`, authMiddleware.auth, ctrl.alterarStatusAgendamento.bind(ctrl));
+router.get(`/agendamentos`,authMiddleware.auth, ctrl.listarAgendamentos.bind(ctrl));
+router.get(`/agendamentos/:id`,authMiddleware.auth, ctrl.buscarAgendamentoPorId.bind(ctrl));
+router.post(`/agendamentos/:id`, authMiddleware.auth, ctrl.cadastrarNovoAgendamento.bind(ctrl));
+router.patch(`/agendamentos/:id`, authMiddleware.auth, ctrl.alterarStatusAgendamento.bind(ctrl));
 
 export default router;
