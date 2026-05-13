b74fca0a7ae2fff8cdf602bbf2254b412f1a6c37
fanhucci <felipeanhucci38@gmail.com>
2026-05-13 00:09:51 -0300

    novo customInput

--- apps/front/app/(protected)/usuarios/page.tsx
+++ apps/front/app/(protected)/usuarios/page.tsx
@@ -13,7 +13,7 @@ import CustomSelect from "@/components/customSelect";
 
 
 export default function UsuariosPage(){
-    const {queryString, filters, handleFilters} = useFilter<UsuarioSearch>({
+    const {queryString, filters, handleFilters, limparFiltros} = useFilter<UsuarioSearch>({
         search:'',
         tipo:"nome",
         id_cargo:1,
@@ -68,7 +68,16 @@ export default function UsuariosPage(){
             </div>
 
             <div className="flex flex-col gap-2 p-4 rounded-xl border border-gray-200 shadow-sm">
-                <div className="flex items-center gap-2 text-xs text-gray-500">Filtros:</div>
+                <div className="flex justify-between p-3">
+                    <div className="flex items-center gap-2 text-xs text-gray-500">Filtros:</div>
+                    <SubmitButton
+                        estilo="fantasma"
+                        isLoading={buttonLoading}
+                        onClick={limparFiltros}
+                    >
+                        Limpar 
+                    </SubmitButton>
+                </div>
                     
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-6 gap-4 items-end">
                     <div className="col-span-1 md:col-span-3">
