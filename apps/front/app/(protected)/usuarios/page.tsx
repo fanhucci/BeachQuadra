f87634c5eb838fa843a720b8b2ed6589515326a4
fanhucci <felipeanhucci38@gmail.com>
2026-05-12 23:37:10 -0300

    novo customInput

--- apps/front/app/(protected)/usuarios/page.tsx
+++ apps/front/app/(protected)/usuarios/page.tsx
@@ -67,10 +67,10 @@ export default function UsuariosPage(){
                 </SubmitButton>
             </div>
 
-            <div className="flex gap-2 p-4 rounded-xl border border-gray-200 shadow-sm">
+            <div className="flex flex-col gap-2 p-4 rounded-xl border border-gray-200 shadow-sm">
                 <div className="flex items-center gap-2 text-xs text-gray-500">Filtros:</div>
                     
-                    <div className="flex flex-wrap items-center gap-3 bg-white ">
+                    <div className="flex flex-row items-center gap-3 bg-white ">
                         <CustomInput
                             name="search"
                             onChange={handleFilters}
