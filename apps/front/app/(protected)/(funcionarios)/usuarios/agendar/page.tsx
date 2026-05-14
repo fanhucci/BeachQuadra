6c8849c81a8cb5b049ee81ee3fbb19239479cd7f
fanhucci <felipeanhucci38@gmail.com>
2026-05-14 03:35:39 -0300

    separação de agendamento em 3 rotas distintas

---
+++ apps/front/app/(protected)/(funcionarios)/usuarios/agendar/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import AgendamentoFormComponent from "@/components/AgendamentoFormComponent/AgendamentoForm"
+
+export default function AgendarClientePage(){
+    return(
+        <AgendamentoFormComponent
+            context="funcionario"
+        />
+    )
+}
