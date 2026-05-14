e1a45b5468ad2b9b49ef37c342b206b41a116e9d
fanhucci <felipeanhucci38@gmail.com>
2026-05-14 20:26:17 -0300

    separação de agendamento em 3 rotas distintas

--- apps/front/app/(protected)/(funcionarios)/usuarios/agendar/[[...id]]/page.tsx
+++ apps/front/app/(protected)/(funcionarios)/usuarios/agendar/[[...id]]/page.tsx
@@ -4,53 +4,55 @@ import AgendamentoFormComponent from "@/components/AgendamentoFormComponent/Agen
 import { apiRequest } from "@/utils/apiHandler";
 import { Perfil } from "@app/shared";
 import { useParams } from "next/navigation";
-import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { toast } from "sonner";
 
 export default function AgendarClientePage(){
-        const {id} = useParams();
-        const router = useRouter();
+    const {id} = useParams();
+     
+    const [perfil,setPerfil] = useState<Perfil|null>(null);
+    const [loading,setLoading] = useState<boolean>(true);
     
-        const [perfil,setPerfil] = useState<Perfil|null>(null);
-        const [loading,setLoading] = useState<boolean>(true);
-    
-        async function pegarUsuario(id:number){
-            try {
-                setLoading(true);
-                const dado = await apiRequest(`/usuarios/${id}`)
-                setPerfil(dado);
-            } catch (error) {
-                toast.error(error instanceof Error? error.message : 'Erro ao carregar usuário');
-            }
-            finally{
-                setLoading(false);
-            }
-            
+    async function pegarUsuario(id:number){
+        try {
+            setLoading(true);
+            const dado = await apiRequest(`/usuarios/${id}`)
+            setPerfil(dado);
+        } catch (error) {
+            toast.error(error instanceof Error? error.message : 'Erro ao carregar usuário');
         }
-    
-        useEffect(()=>{
-            const idInvalido = isNaN(Number(id));
+        finally{
+            setLoading(false);
+        }
             
-            if(!idInvalido){ 
-               pegarUsuario(Number(id));
-            }
-        },[id])
+    }
     
-        if(loading){
+    useEffect(()=>{
+        const idInvalido = isNaN(Number(id));
+            
+        if(!idInvalido){ 
+           pegarUsuario(Number(id));
+        }
+    },[id])
+    
+    if(loading){
             return (<>Carregando...</>)
-        }
+    }
     
-        if(!perfil) {
-            router.replace('/login');
-            return null;
-        }
-    
+    if(!perfil){
+        return(
+            <AgendamentoFormComponent
+                context="funcionario"
+            />
+        )
+    }
+    else{
+        return(
+            <AgendamentoFormComponent
+                context="funcionario"
+                clientePreSelecionado={perfil.usuario}
+            />
+        )
+    }
 
-    return(
-        <AgendamentoFormComponent
-            context="funcionario"
-            clientePreSelecionado={perfil.usuario}
-        />
-    )
 }
