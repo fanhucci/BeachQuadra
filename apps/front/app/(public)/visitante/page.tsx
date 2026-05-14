6fef7fd2ce314ffd42be3c1446e101f66470f2c1
fanhucci <felipeanhucci38@gmail.com>
2026-05-14 02:26:27 -0300

    teste de pagina de agenda

--- apps/front/app/(public)/visitante/page.tsx
+++ apps/front/app/(public)/visitante/page.tsx
@@ -3,7 +3,7 @@ import AgendaPage from "@/app/(protected)/agenda/page";
 export default function VisitantePage(){
     return(
         <AgendaPage
-            context="visitante"
+            context="funcionario"
         />
     )
 }
