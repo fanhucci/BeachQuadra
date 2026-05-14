4f101405ef6234e916a36cfb8b2d1a793bf18b37
fanhucci <felipeanhucci38@gmail.com>
2026-05-14 19:29:17 -0300

    separação de agendamento em 3 rotas distintas

--- apps/front/components/AgendamentoFormComponent/AgendamentoForm.tsx
+++ apps/front/components/AgendamentoFormComponent/AgendamentoForm.tsx
@@ -151,7 +151,7 @@ function SalvarAgendamentoForm({
         <div className="flex flex-col justify-center items-center">
             <>
                 {
-                    JSON.stringify(clientePreSelecionado.nome)
+                    JSON.stringify(clientePreSelecionado)
                 }
                     <SubmitButton
                         estilo="primario"
