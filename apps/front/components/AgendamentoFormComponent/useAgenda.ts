9fe1ef51aafc79ef42100cd3fccde5ef25c12b46
fanhucci <felipeanhucci38@gmail.com>
2026-05-15 04:37:38 -0300

    teste de horarios salvos no localStorage

--- apps/front/components/AgendamentoFormComponent/useAgenda.ts
+++ apps/front/components/AgendamentoFormComponent/useAgenda.ts
@@ -1,219 +1,225 @@
-    'use client'
+'use client'
 
-    import { apiRequest } from "@/utils/apiHandler"
+import { apiRequest } from "@/utils/apiHandler"
 import { useRouter } from "next/navigation";
-    import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { toast } from "sonner";
 
-    type TiposQuadra = 'individual'|'duplas';
+type TiposQuadra = 'individual'|'duplas';
 
-    type QuadraDisponivel = {
-        id_quadra:number;
-        tipo:TiposQuadra;
-        valor:number;
+type QuadraDisponivel = {
+    id_quadra:number;
+    tipo:TiposQuadra;
+    valor:number;
+}
+
+export type SlotHorario = {
+    horario:Date;
+    permitido:boolean;
+    id_agendamento?:number;
+    disponivel:QuadraDisponivel[];
+}
+
+export type HorarioSelecionado = {
+    horario:Date;
+    quadra:QuadraDisponivel;
+}
+
+export default function useAgenda(){
+
+    const hoje = new Date();
+    hoje.setHours(0, 0, 0, 0);
+    const diaSemana = hoje.getDay(); 
+    const diferencaParaSegunda = diaSemana === 0 ? 6 : diaSemana - 1;
+    const segundaFeira = new Date(hoje);
+    segundaFeira.setDate(hoje.getDate() - diferencaParaSegunda);
+
+    const router = useRouter();
+        
+    const [data,setData] = useState<Date>(new Date(segundaFeira));
+    const [dados, setDados] = useState<SlotHorario[]>([]);
+    const [tipo,setTipo] = useState<TiposQuadra>('individual');
+    const [horarioSelecionado,setHorarioSelecionado] = useState<HorarioSelecionado[]>([]);
+    const [loading,setLoading] = useState<boolean>(false);
+
+    async function carregarDiasLivres() {
+        try {
+            setLoading(true);
+            const slots = await apiRequest(`/horario-disponivel?data=${data.toISOString()}&tipo=${tipo}`);
+            setDados(slots);
+        } catch (error) {
+            toast.error(error instanceof Error? error.message : 'Erro ao carregar horarios.');
+        }
+        finally{
+            setLoading(false);
+        }
+    }   
+
+    async function salvarReservas(id:number){
+        const reservas = horarioSelecionado.map(r=>({
+            id_quadra:r.quadra.id_quadra,
+            horario:r.horario
+        }));
+
+        const payload = {
+            id_pessoa:id,
+            reservas:reservas
+        }
+
+        try {
+            setLoading(true);
+            const agendamento = await apiRequest(`/agendamentos`,{
+                method:'POST',
+                body:JSON.stringify(payload)
+            })
+            toast.success('Horários agendados com sucesso.');
+            router.replace(`/agendamentos/${agendamento}`);
+        } catch (error) {
+            toast.error(error instanceof Error? error.message : 'Erro ao salvar agendamento.');
+        }
+        finally{
+            setLoading(false);
+        }
     }
 
-    export type SlotHorario = {
-        horario:Date;
-        permitido:boolean;
-        id_agendamento?:number;
-        disponivel:QuadraDisponivel[];
+    const selecionarHorario = (slot: SlotHorario) => {
+
+        setHorarioSelecionado((prev) => {
+
+            const quadraAtual = slot.disponivel[0]; 
+            if (!quadraAtual) return prev;
+
+            const jaSelecionado = prev.find(
+                (r) => 
+                    new Date(r.horario).getTime() === new Date(slot.horario).getTime() &&
+                    r.quadra.id_quadra === quadraAtual.id_quadra
+            );
+
+            if (jaSelecionado) {
+                return prev.filter(r => 
+                    !(new Date(r.horario).getTime() === new Date(slot.horario).getTime() && 
+                    r.quadra.id_quadra === quadraAtual.id_quadra)
+                );
+            }
+
+
+            return [
+                ...prev,
+                {
+                    horario: slot.horario,
+                    quadra: quadraAtual
+                }
+            ];
+        });
     }
 
-    export type HorarioSelecionado = {
-        horario:Date;
-        quadra:QuadraDisponivel;
+    const removerHorarioSelecionado = (slotHorario: HorarioSelecionado) => {
+        setHorarioSelecionado((prev) => 
+            prev.filter(r => 
+                new Date(r.horario).getTime() !== new Date(slotHorario.horario).getTime() || 
+                r.quadra.id_quadra !== slotHorario.quadra.id_quadra
+            )
+        );
+    };
+
+    const proximaSemana = ()=>{
+        const proximaSemana = new Date(data);
+
+        proximaSemana.setDate(proximaSemana.getDate()+7);
+
+        setData(proximaSemana);
+    }
+        
+    const semanaAnterior = ()=>{
+        const novaData = new Date(data);
+
+        novaData.setDate(novaData.getDate()-7)
+
+        if (novaData.getTime()>=segundaFeira.getTime()) {
+            setData(novaData);
+        }
     }
 
-    export default function useAgenda(){
-
-        const hoje = new Date();
-        hoje.setHours(0, 0, 0, 0);
-        const diaSemana = hoje.getDay(); 
-        const diferencaParaSegunda = diaSemana === 0 ? 6 : diaSemana - 1;
-        const segundaFeira = new Date(hoje);
-        segundaFeira.setDate(hoje.getDate() - diferencaParaSegunda);
-
-        const router = useRouter();
+    useEffect(()=>{
         const horariosNaMemoria = localStorage.getItem('memoria');
         
-        const [data,setData] = useState<Date>(new Date(segundaFeira));
-        const [dados, setDados] = useState<SlotHorario[]>([]);
-        const [tipo,setTipo] = useState<TiposQuadra>('individual');
-        const [horarioSelecionado,setHorarioSelecionado] = useState<HorarioSelecionado[]>([]);
-        const [loading,setLoading] = useState<boolean>(false);
-
-        async function carregarDiasLivres() {
-            try {
-                setLoading(true);
-                const slots = await apiRequest(`/horario-disponivel?data=${data.toISOString()}&tipo=${tipo}`);
-                setDados(slots);
-            } catch (error) {
-                toast.error(error instanceof Error? error.message : 'Erro ao carregar horarios.');
-            }
-            finally{
-                setLoading(false);
-            }
-        }   
-
-        async function salvarReservas(id:number){
-            const reservas = horarioSelecionado.map(r=>({
-                id_quadra:r.quadra.id_quadra,
-                horario:r.horario
-            }));
-
-            const payload = {
-                id_pessoa:id,
-                reservas:reservas
-            }
-
-            try {
-                setLoading(true);
-                const agendamento = await apiRequest(`/agendamentos`,{
-                    method:'POST',
-                    body:JSON.stringify(payload)
-                })
-                toast.success('Horários agendados com sucesso.');
-                router.replace(`/agendamentos/${agendamento}`);
-            } catch (error) {
-                toast.error(error instanceof Error? error.message : 'Erro ao salvar agendamento.');
-            }
-            finally{
-                setLoading(false);
-            }
+        if(!horariosNaMemoria){
+            carregarDiasLivres();
         }
 
-        const selecionarHorario = (slot: SlotHorario) => {
+    },[tipo,data]);
 
-            setHorarioSelecionado((prev) => {
 
-                const quadraAtual = slot.disponivel[0]; 
-                if (!quadraAtual) return prev;
+    const sincronizarHorariosMemoria = async () => {
+        const horariosNaMemoria = localStorage.getItem('memoria');
 
-                const jaSelecionado = prev.find(
-                    (r) => 
-                        new Date(r.horario).getTime() === new Date(slot.horario).getTime() &&
-                        r.quadra.id_quadra === quadraAtual.id_quadra
+        if(!horariosNaMemoria){
+            return;
+        }
+
+        try {
+            setLoading(true);
+
+            const dadosAtuais = await apiRequest(`/horario-disponivel?data=${data.toISOString()}&tipo=${tipo}`);
+
+            if (!dadosAtuais) return; 
+
+            setDados(dadosAtuais); 
+
+            const horariosRascunho = JSON.parse(horariosNaMemoria) as HorarioSelecionado[];
+
+            const horariosValidados = horariosRascunho.filter((rascunho) => {
+                const slotCorrespondente = dadosAtuais.find(
+                    (slot:SlotHorario) => new Date(slot.horario).getTime() === new Date(rascunho.horario).getTime()
                 );
 
-                if (jaSelecionado) {
-                    return prev.filter(r => 
-                        !(new Date(r.horario).getTime() === new Date(slot.horario).getTime() && 
-                        r.quadra.id_quadra === quadraAtual.id_quadra)
-                    );
-                }
+                if (!slotCorrespondente) return false;
 
+                const quadraAindaDisponivel = slotCorrespondente.disponivel.some(
+                    (quadra:QuadraDisponivel) => quadra.id_quadra === rascunho.quadra.id_quadra
+                );
 
-                return [
-                    ...prev,
-                    {
-                        horario: slot.horario,
-                        quadra: quadraAtual
-                    }
-                ];
+                return quadraAindaDisponivel;
             });
-        }
 
-        const removerHorarioSelecionado = (slotHorario: HorarioSelecionado) => {
-            setHorarioSelecionado((prev) => 
-                prev.filter(r => 
-                    new Date(r.horario).getTime() !== new Date(slotHorario.horario).getTime() || 
-                    r.quadra.id_quadra !== slotHorario.quadra.id_quadra
-                )
-            );
-        };
+            setHorarioSelecionado(horariosValidados);
 
-        const proximaSemana = ()=>{
-            const proximaSemana = new Date(data);
-
-            proximaSemana.setDate(proximaSemana.getDate()+7);
-
-            setData(proximaSemana);
-        }
-        
-        const semanaAnterior = ()=>{
-            const novaData = new Date(data);
-
-            novaData.setDate(novaData.getDate()-7)
-
-            if (novaData.getTime()>=segundaFeira.getTime()) {
-                setData(novaData);
-            }
-        }
-
-        useEffect(()=>{
-
-            if(!horariosNaMemoria){
-                carregarDiasLivres();
+            if (horariosValidados.length > 0) {
+                toast.success('Horarios recuperados com sucesso.');
             }
 
-        },[tipo,data]);
-
-
-        const sincronizarHorariosMemoria = async () => {
-            try {
-                setLoading(true);
-
-                const dadosAtuais = await apiRequest(`/horario-disponivel?data=${data.toISOString()}&tipo=${tipo}`);
-
-                if (!dadosAtuais) return; 
-
-                setDados(dadosAtuais); 
-
-                const horariosRascunho = JSON.parse(horariosNaMemoria!) as HorarioSelecionado[];
-
-                const horariosValidados = horariosRascunho.filter((rascunho) => {
-                    const slotCorrespondente = dadosAtuais.find(
-                        (slot:SlotHorario) => new Date(slot.horario).getTime() === new Date(rascunho.horario).getTime()
-                    );
-
-                    if (!slotCorrespondente) return false;
-
-                    const quadraAindaDisponivel = slotCorrespondente.disponivel.some(
-                        (quadra:QuadraDisponivel) => quadra.id_quadra === rascunho.quadra.id_quadra
-                    );
-
-                    return quadraAindaDisponivel;
-                });
-
-                setHorarioSelecionado(horariosValidados);
-
-                if (horariosValidados.length > 0) {
-                    toast.success('Horarios recuperados com sucesso.');
-                }
-
-                if (horariosValidados.length < horariosRascunho.length) {
-                    toast.warning("Algum dos horários selecionados foi reservado recentemente e removido da lista.");
-                }
-
-            } catch (error) {
-                toast.error('Não foi possivel recuperar os horarios.');
-            } finally {
-                setLoading(false);
-                localStorage.removeItem('memoria');
+            if (horariosValidados.length < horariosRascunho.length) {
+                toast.warning("Algum dos horários selecionados foi reservado recentemente e removido da lista.");
             }
-        };
 
-        useEffect(() => {
-            if (!horariosNaMemoria) return;       
+        } catch (error) {
+            toast.error('Não foi possivel recuperar os horarios.');
+        } finally {
+            setLoading(false);
+            localStorage.removeItem('memoria');
+        }
+    };
 
-            sincronizarHorariosMemoria();
-        }, []);
+    useEffect(() => {
+        if (!horariosNaMemoria) return;       
 
-        const valorTotal = horarioSelecionado.reduce((acc,item)=> acc + Number(item.quadra.valor),0);
+        sincronizarHorariosMemoria();
+    }, []);
 
-        return {
-            dados,
-            valorTotal,
-            horarioSelecionado,
-            tipo,
-            loading,
-            selecionarHorario,
-            salvarReservas,
-            proximaSemana,
-            semanaAnterior,
-            setTipo,
-            removerHorarioSelecionado
+    const valorTotal = horarioSelecionado.reduce((acc,item)=> acc + Number(item.quadra.valor),0);
 
-        };
-    }
+    return {
+        dados,
+        valorTotal,
+        horarioSelecionado,
+        tipo,
+        loading,
+        selecionarHorario,
+        salvarReservas,
+        proximaSemana,
+        semanaAnterior,
+        setTipo,
+        removerHorarioSelecionado
+
+    };
+}
