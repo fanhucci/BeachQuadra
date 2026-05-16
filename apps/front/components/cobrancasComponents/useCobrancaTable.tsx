cb3d70321dd26f894d3e98171e54eb3373ea98da
fanhucci <felipeanhucci38@gmail.com>
2026-05-15 22:07:17 -0300

    criação de pagina de cobranças

--- apps/front/components/cobrancasComponents/useCobrancaTable.tsx
+++ apps/front/components/cobrancasComponents/useCobrancaTable.tsx
@@ -16,7 +16,7 @@ export default function useCobrancaTable(acoes:AcoesCobranca){
         { key: "id_pessoa", label: "Cliente", 
             render:(_,c:Cobranca)=>(
                 <LinkButton
-
+                    className="w-fit bg-none"
                     estilo="secundario"
                     href={`/usuarios/${c.id_pessoa}`}
                 >
