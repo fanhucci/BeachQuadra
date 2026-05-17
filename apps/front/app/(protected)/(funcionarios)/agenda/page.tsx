6f23d5c360744f2d80341c011b548a47d5a84108
fanhucci <felipeanhucci38@gmail.com>
2026-05-17 05:01:34 -0300

    prototipagem pagina de dashboard

--- apps/front/app/(protected)/(funcionarios)/agenda/page.tsx
+++ apps/front/app/(protected)/(funcionarios)/agenda/page.tsx
@@ -3,6 +3,7 @@ import Agenda from "@/components/agenda";
 import CustomModal from "@/components/customModal";
 import { apiRequest } from "@/utils/apiHandler";
 import { useEffect, useState } from "react";
+import { toast } from "sonner";
 
 export default function AgendaPage(){
     const [modalOn,setModalOn] = useState<boolean>(false);
@@ -12,9 +13,10 @@ export default function AgendaPage(){
     async function carregarHorarios(){
         try {
             setLoading(true);
-        } catch (error) {
             const dados = await apiRequest(`/horario/agenda`);
             setHorarios(dados);
+        } catch (error) {
+            toast.error(error instanceof Error? error.message : `Erro ao carregar agenda.`)
         }
         finally{
             setLoading(false);
