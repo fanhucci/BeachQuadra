386f3dd04d7ba2ae2ed3f5248c7c6c17a2388a4a
fanhucci <felipeanhucci38@gmail.com>
2026-05-17 05:50:10 -0300

    prototipagem pagina de dashboard

--- apps/front/app/(protected)/(funcionarios)/agenda/page.tsx
+++ apps/front/app/(protected)/(funcionarios)/agenda/page.tsx
@@ -57,18 +57,17 @@ export default function AgendaPage(){
             >   
                 <div className="flex flex-col gap-4 p-2">
                         
-                    {slotSelecionado?.agendamento && (
-                            <div className="bg-amber-50 border border-amber-200 rounded-lg p-4">
+                    {slotSelecionado?.agendamentos.map(a=>(
+                        <div className="bg-amber-50 border border-amber-200 rounded-lg p-4">
                                 <h3 className="text-amber-800 font-semibold mb-2">Horário Reservado</h3>
                                 <div className="space-y-1 text-sm text-zinc-700">
-                                    <p><strong>ID Agendamento:</strong> {slotSelecionado.agendamento.id_agendamento}</p>
-                                    <p><strong>Cliente:</strong> {slotSelecionado.agendamento.nome}</p>
-                                    <p><strong>CPF:</strong> {slotSelecionado.agendamento.cpf}</p>
-                                    <p><strong>Email:</strong> {slotSelecionado.agendamento.email}</p>
+                                    <p><strong>ID Agendamento:</strong> {a.id_agendamento}</p>
+                                    <p><strong>Cliente:</strong> {a.nome}</p>
+                                    <p><strong>CPF:</strong> {a.cpf}</p>
+                                    <p><strong>Email:</strong> {a.email}</p>
                                 </div>
                             </div>
-                        ) 
-                    }
+                    ))}
 
                 </div>
             </CustomModal>
