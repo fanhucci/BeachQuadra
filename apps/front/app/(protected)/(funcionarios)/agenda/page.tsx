7a9a6e1fbf86e863dbf5e15e35372cbda9def447
fanhucci <felipeanhucci38@gmail.com>
2026-05-17 16:13:47 -0300

    atualização de pagina de agenda

--- apps/front/app/(protected)/(funcionarios)/agenda/page.tsx
+++ apps/front/app/(protected)/(funcionarios)/agenda/page.tsx
@@ -63,7 +63,7 @@ export default function AgendaPage(){
                     }
                 ]}
             >   
-                <div className="flex flex-col gap-4 p-2">
+                <div className="flex flex-col w-200 h-200 overflow-y-scroll gap-4 p-2">
                         
                     {slotSelecionado?.agendamentos.map(a=>(
                         <div className="bg-amber-50 border border-amber-200 rounded-lg p-4">
