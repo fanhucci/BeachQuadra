219014e3c57b2d4c32b9be2fdfb5c291237dc299
fanhucci <felipeanhucci38@gmail.com>
2026-05-17 05:23:17 -0300

    prototipagem pagina de dashboard

--- apps/front/components/agenda.tsx
+++ apps/front/components/agenda.tsx
@@ -60,7 +60,7 @@ export default function Agenda({
         
                     const isPassado = dataSemFuso.getTime() < agora.getTime();
 
-                    const temReserva = slot.agendamento.id_agendamento !==null;
+                    const temReserva = slot.agendamento?.id_agendamento !==null;
   
                     const blocked = contexto === 'agenda' 
                         ? !slot.permitido 
@@ -81,7 +81,7 @@ export default function Agenda({
                                 title={horaFormatada}
                                 isBlocked={blocked}
                                 isAvaliable={avaliable}
-                                hasAgendamento={!!slot.agendamento.id_agendamento}
+                                hasAgendamento={!!slot.agendamento?.id_agendamento}
                                 isSelected={isSelected}
                                 action={(temReserva || contexto ==='reserva') ? () => aoSelecionar?.(slot) : undefined}
                             />
