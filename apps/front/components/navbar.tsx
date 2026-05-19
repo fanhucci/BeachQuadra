ab31b87da959c7d6f7fb3f32c2042371aca269fe
fanhucci <felipeanhucci38@gmail.com>
2026-05-19 17:43:42 -0300

    teste de navbar

--- apps/front/components/navbar.tsx
+++ apps/front/components/navbar.tsx
@@ -160,7 +160,7 @@ export function EmployeeSidebar() {
       {!isOpen && (
         <SubmitButton
           estilo="fantasma"
-          className="fixed top-4 right-0 z-40 bg-gray-900! p-2.5! rounded-xl text-white hover:bg-gray-800 shadow-md"
+          className="fixed top-4 right-0 z-40 bg-gray-900! p-2.5! rounded-l-xl text-white hover:bg-gray-800 shadow-md"
           onClick={() => setIsOpen(true)}
         >
           <ChevronLeft className="h-5 w-5" />
