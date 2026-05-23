baca7d1f0214b2e24f3aa42c0625e8839137492f
fanhucci <felipeanhucci38@gmail.com>
2026-05-22 22:56:20 -0300

    teste de exportação para pdf

--- apps/front/app/(protected)/(funcionarios)/usuarios/page.tsx
+++ apps/front/app/(protected)/(funcionarios)/usuarios/page.tsx
@@ -65,6 +65,25 @@ export default function UsuariosPage(){
     const totalGeral = dados[0]?.total_geral ?? 0;
     const naoTemProximaPagina = (page * limit) >= totalGeral;
 
+    const totaisTela = dados.reduce(
+    (acc, item) => {
+        const valorNum = Number(item.valor) || 0;
+        
+        if(item.status.toLowerCase() === "concluido" || item.status.toLowerCase() === "pendente" ){
+            acc.totalGeral += valorNum;
+        }
+            
+        if (item.status.toLowerCase() === "concluido") {
+            acc.totalRecebido += valorNum;
+        } else if (item.status.toLowerCase() === "pendente") {
+            acc.totalPendente += valorNum;
+        }
+
+        return acc;
+    },
+    { totalGeral: 0, totalRecebido: 0, totalPendente: 0 }
+);
+
     return(
         <section className="w-full h-screen bg-gray-50/50 overflow-hidden flex flex-col">
             <div className="mx-auto w-full max-w-7xl 2xl:max-w-[1600px] h-full min-h-0 p-4 sm:p-6 lg:p-8 flex flex-col gap-4">
@@ -106,6 +125,31 @@ export default function UsuariosPage(){
                         types={filters}
                         handle={handleFilters}
                     />
+                </section>
+                <section className="grid grid-cols-1 sm:grid-cols-3 gap-4 flex-shrink-0">
+
+                    <div className="bg-white p-4 rounded-2xl border border-gray-200 shadow-sm flex flex-col gap-1">
+                        <span className="text-xs font-bold text-gray-400 uppercase tracking-wider">Total Faturado (Página)</span>
+                        <span className="text-xl font-bold text-gray-900">
+                            R$ {totaisTela.totalGeral.toLocaleString("pt-BR", { minimumFractionDigits: 2 })}
+                        </span>
+                    </div>
+
+                    {/* Card Total Recebido */}
+                    <div className="bg-white p-4 rounded-2xl border border-gray-200 shadow-sm flex flex-col gap-1">
+                        <span className="text-xs font-bold text-gray-400 uppercase tracking-wider">Total Recebido</span>
+                        <span className="text-xl font-bold text-green-600">
+                            R$ {totaisTela.totalRecebido.toLocaleString("pt-BR", { minimumFractionDigits: 2 })}
+                        </span>
+                    </div>
+
+                    {/* Card Total Pendente */}
+                    <div className="bg-white p-4 rounded-2xl border border-gray-200 shadow-sm flex flex-col gap-1">
+                        <span className="text-xs font-bold text-gray-400 uppercase tracking-wider">Total Pendente</span>
+                        <span className="text-xl font-bold text-red-600">
+                            R$ {totaisTela.totalPendente.toLocaleString("pt-BR", { minimumFractionDigits: 2 })}
+                        </span>
+                    </div>
                 </section>
 
                 <section className="bg-white rounded-2xl border border-gray-200 shadow-sm overflow-hidden flex flex-col flex-1 min-h-0">
