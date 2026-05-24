cc8d65a8ff2989604b7419e131d04fff32be5e2f
fanhucci <felipeanhucci38@gmail.com>
2026-05-24 04:46:21 -0300

    teste de driver.js

--- apps/front/app/(protected)/(both)/agendamentos/[id]/layout.tsx
+++ apps/front/app/(protected)/(both)/agendamentos/[id]/layout.tsx
@@ -2,11 +2,15 @@
 
 import SemAutorizacao from "@/components/erros/semAutorizacao";
 import { useUser } from "@/context/userContext";
+import { useParams } from "next/navigation";
 
 export default function EmployeeLayout({children}:{children:React.ReactNode}){
-    const {user} = useUser()
+    const {user} = useUser();
+    const {id} = useParams();
 
-    if(!user || user.id_cargo == 1) return <SemAutorizacao/>
+    if (!user || (user.id_pessoa != Number(id) && user.id_cargo != 1)) {
+        return <SemAutorizacao />;
+    }
 
     return(
         <>
--- apps/front/app/(protected)/layout.tsx
+++ apps/front/app/(protected)/layout.tsx
@@ -3,7 +3,6 @@ import AuthGuard from "@/utils/authGuard";
 
 export default function AuthLayout({children}:{children:React.ReactNode}){
 
-
     return(
         <AuthGuard>
             <Navbar />
