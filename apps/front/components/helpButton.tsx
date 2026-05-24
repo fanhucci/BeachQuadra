6cf0231165e468f7f3d902efa253a7e71b78dcb3
fanhucci <felipeanhucci38@gmail.com>
2026-05-24 02:12:02 -0300

    teste de driver.js

--- apps/front/components/helpButton.tsx
+++ apps/front/components/helpButton.tsx
@@ -2,6 +2,7 @@
 'use client'
 import { driver } from "driver.js";
 import "driver.js/dist/driver.css";
+import { BadgeQuestionMark } from "lucide-react";
 
 
 interface Step {
@@ -26,7 +27,7 @@ export default function HelpButton({ steps }: { steps: Step[] }) {
       onClick={startTutorial}
       className="fixed bottom-6 right-6 z-50 p-4 bg-indigo-600 text-white rounded-full shadow-lg hover:bg-indigo-700 transition-all hover:scale-105"
     >
-      ?
+      <span><BadgeQuestionMark size={20}/></span>
     </button>
   );
 }
