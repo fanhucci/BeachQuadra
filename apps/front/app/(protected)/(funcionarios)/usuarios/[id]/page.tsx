5f71f2ca2a8172c6886021abd2e3468ae936c6c1
fanhucci <felipeanhucci38@gmail.com>
2026-05-23 22:49:48 -0300

    exportação de histórico

--- apps/front/app/(protected)/(funcionarios)/usuarios/[id]/page.tsx
+++ apps/front/app/(protected)/(funcionarios)/usuarios/[id]/page.tsx
@@ -4,24 +4,24 @@ import PerfilPage from "@/components/perfil/perfilPage";
 import { useEffect, useState } from "react";
 import NaoEncontrado from "@/components/erros/naoEncontrado";
 
-
 export default function UsuarioPerfilPage(){
 
     const {id} = useParams();
-    const [idUsuario,setIdUsuario] = useState<number|null>(null);
+    const [idUsuario, setIdUsuario] = useState<number|null>(null);
 
     useEffect(()=>{
         const idNumerico = Number(id);
         
-        if(!isNaN(idNumerico) && idNumerico>0){
+        if(!isNaN(idNumerico) && idNumerico > 0){
             setIdUsuario(idNumerico);
         }
     },[id])
     
-
     if(!idUsuario) return <NaoEncontrado/>
 
     return (
-        <PerfilPage id_perfil={idUsuario}/>
+        <div className="flex flex-1 flex-col min-h-0 w-full overflow-hidden">
+            <PerfilPage id_perfil={idUsuario}/>
+        </div>
     )
 }
