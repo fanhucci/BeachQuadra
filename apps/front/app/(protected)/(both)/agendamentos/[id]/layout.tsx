a02fdfe704485039efdd963ab901026f7b3f449a
fanhucci <felipeanhucci38@gmail.com>
2026-05-24 04:57:45 -0300

    teste de driver.js

--- apps/front/app/(protected)/(both)/agendamentos/[id]/layout.tsx
+++ apps/front/app/(protected)/(both)/agendamentos/[id]/layout.tsx
@@ -7,7 +7,10 @@ import { useParams } from "next/navigation";
 export default function EmployeeLayout({children}:{children:React.ReactNode}){
     const {user} = useUser();
     const {id} = useParams();
-
+    console.log(user?.id_pessoa)
+    console.log(user?.id_cargo)
+    console.log(Number(id));
+    console.log(!user || (user.id_cargo === 1 && user.id_pessoa !== Number(id)))
     if (!user || (user.id_cargo === 1 && user.id_pessoa !== Number(id))) {
         return <SemAutorizacao />;
     }
