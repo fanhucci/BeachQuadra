530fd49e4bd70ace442b160940c49e282d715c2c
fanhucci <felipeanhucci38@gmail.com>
2026-05-24 19:39:53 -0300

    teste de driver.js

---
+++ apps/front/app/(erros)/sem-permissao/page.tsx
@@ -0,0 +1,8 @@
+'use client'
+import SemAutorizacao from "@/components/erros/semAutorizacao";
+
+export default function SemPermissaoPage(){
+    return(
+        <SemAutorizacao/>
+    )
+}
--- apps/front/utils/apiHandler.ts
+++ apps/front/utils/apiHandler.ts
@@ -19,6 +19,12 @@ export async function apiRequest(endpoint: string, options: RequestInit = {}) {
         : null;
 
     if (!response.ok) {
+
+        if (response.status === 403) {
+            window.location.href = '/sem-permissao';
+            return;
+        }
+
         throw new Error(data?.erro || `Erro HTTP ${response.status}`);
     }
 
