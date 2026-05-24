1a23c18f38a76d904e5839e4c4c2e89cae3c4dbb
fanhucci <felipeanhucci38@gmail.com>
2026-05-24 01:21:50 -0300

    exportação de agenda

--- apps/front/app/(protected)/(funcionarios)/quadras/[id]/useQuadraDetail.ts
+++ apps/front/app/(protected)/(funcionarios)/quadras/[id]/useQuadraDetail.ts
@@ -1,63 +1,63 @@
-'use client'
+// 'use client'
 
-import { apiRequest } from "@/utils/apiHandler";
-import { Quadra } from "@app/shared";
-import { useParams, useRouter } from "next/navigation";
-import { useEffect, useState } from "react"
-import { toast } from "sonner";
+// import { apiRequest } from "@/utils/apiHandler";
+// import { Quadra } from "@app/shared";
+// import { useParams, useRouter } from "next/navigation";
+// import { useEffect, useState } from "react"
+// import { toast } from "sonner";
 
-export type Slot = {
-    id_agendamento:number;
-    horario:Date;
-    permitido:boolean;
-    quadras:number[];
-}
+// export type Slot = {
+//     id_agendamento:number;
+//     horario:Date;
+//     permitido:boolean;
+//     quadras:number[];
+// }
 
-export default function useQuadraDetail(){
-    const {id} = useParams();
-    const [loading,setLoading] = useState<boolean>(false);
-    const [quadra,setQuadra] = useState<Quadra[]|null>(null);
-    const router = useRouter();
+// export default function useQuadraDetail(){
+//     const {id} = useParams();
+//     const [loading,setLoading] = useState<boolean>(false);
+//     const [quadra,setQuadra] = useState<Quadra[]|null>(null);
+//     const router = useRouter();
 
 
-    async function carregarDados(){
+//     async function carregarDados(){
 
-        const idQuadra = Number(id);
+//         const idQuadra = Number(id);
         
-        if(isNaN(idQuadra)){
-            toast.error(`ID inválido`);
-            return;
-        }
+//         if(isNaN(idQuadra)){
+//             toast.error(`ID inválido`);
+//             return;
+//         }
 
-        try {
-            setLoading(true);
+//         try {
+//             setLoading(true);
 
-            const dados = await apiRequest(`/horario-disponivel/${idQuadra}`);
+//             const dados = await apiRequest(`/horario-disponivel/${idQuadra}`);
 
-            setQuadra(dados);
+//             setQuadra(dados);
 
-        } 
-        catch(error){
-            toast.error(error instanceof Error? error.message : "Erro inesperado");
-        }
-        finally{
-            setLoading(false);
-        }
-    }
+//         } 
+//         catch(error){
+//             toast.error(error instanceof Error? error.message : "Erro inesperado");
+//         }
+//         finally{
+//             setLoading(false);
+//         }
+//     }
 
-    const mostrarAgendamento = (slot:Slot)=>{
-        router.push(`/agendamentos/${slot.agendamentos[0].id_agendamento}`);
-    }
+//     const mostrarAgendamento = (slot:Slot)=>{
+//         router.push(`/agendamentos/${slot.agendamentos[0].id_agendamento}`);
+//     }
 
-    useEffect(()=>{
-        if(id){
-            carregarDados();
-        }
-    },[id])
+//     useEffect(()=>{
+//         if(id){
+//             carregarDados();
+//         }
+//     },[id])
 
-    return{
-        loading,
-        quadra,
-        mostrarAgendamento
-    }
-}
+//     return{
+//         loading,
+//         quadra,
+//         mostrarAgendamento
+//     }
+// }
