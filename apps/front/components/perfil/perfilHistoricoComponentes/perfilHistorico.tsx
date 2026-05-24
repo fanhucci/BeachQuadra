9a076364856355d2c9f5ae2e272d373d5bbd37b2
fanhucci <felipeanhucci38@gmail.com>
2026-05-23 22:09:51 -0300

    exportação de histórico

--- apps/front/components/perfil/perfilHistoricoComponentes/perfilHistorico.tsx
+++ apps/front/components/perfil/perfilHistoricoComponentes/perfilHistorico.tsx
@@ -127,7 +127,7 @@ export default function PerfilHistorico({ id_usuario }: { id_usuario: number }) 
 
     return (
         <section className="max-w-7xl mx-auto p-4 sm:p-6 lg:p-8 w-full transition-all flex flex-col flex-1 min-h-0">
-            <div className="w-full h-full min-h-0 p-4 sm:p-6 lg:p-8 flex flex-col gap-4 overflow-hidden">
+            <div className="w-full flex-1 min-h-0 p-4 sm:p-6 lg:p-8 flex flex-col gap-4 overflow-hidden">
 
                 <header className="flex flex-col sm:flex-row sm:items-center sm:justify-between gap-4 border-b border-gray-200 pb-4 flex-shrink-0">
                     <div className="flex items-center gap-4">
