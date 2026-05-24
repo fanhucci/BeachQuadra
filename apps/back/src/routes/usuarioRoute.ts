d2f7a7a712101a033d7cf7ec32e3e3b7a6890dd4
fanhucci <felipeanhucci38@gmail.com>
2026-05-24 20:47:56 -0300

    atualização de histórico

--- apps/back/src/routes/usuarioRoute.ts
+++ apps/back/src/routes/usuarioRoute.ts
@@ -14,13 +14,14 @@ router.post(`/usuarios/proprio`, ctrl.adicionarUsuarioProprio.bind(ctrl));
 
 //clientes
 router.get(`/usuarios/perfil`, authMiddleware.auth, ctrl.listarUsuarioPerfil.bind(ctrl));
+router.get(`/usuarios/:id`, authMiddleware.auth, ctrl.listarUsuarioPorId.bind(ctrl));
 router.get(`/usuarios/:id/historico`, authMiddleware.auth, ctrl.listarHistorico.bind(ctrl));
 router.patch(`/usuarios/:id`, authMiddleware.auth, ctrl.editarUsuario.bind(ctrl));
 
 //administrativo
 router.get(`/usuarios`, authMiddleware.auth, RoleMiddleware.check(Permissao.CLIENTES), ctrl.listarUsuarios.bind(ctrl));
 router.get(`/usuarios/clientes`, authMiddleware.auth, RoleMiddleware.check(Permissao.CLIENTES), ctrl.buscarClientes.bind(ctrl));
-router.get(`/usuarios/:id`, authMiddleware.auth, RoleMiddleware.check(Permissao.CLIENTES), ctrl.listarUsuarioPorId.bind(ctrl));
+
 router.post(`/usuarios`, authMiddleware.auth, RoleMiddleware.check(Permissao.CLIENTES), ctrl.adicionarUsuario.bind(ctrl));
 router.patch(`/usuarios/:id/ativar`, authMiddleware.auth, RoleMiddleware.check(Permissao.CLIENTES), ctrl.ativarUsuario.bind(ctrl));
 router.patch(`/usuarios/:id/desativar`, authMiddleware.auth, RoleMiddleware.check(Permissao.CLIENTES), ctrl.desativarUsuario.bind(ctrl));
