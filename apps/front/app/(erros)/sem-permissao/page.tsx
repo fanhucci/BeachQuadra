00efef58bd5e51e9ab5aa0f3d98d64b911487658
fanhucci <felipeanhucci38@gmail.com>
2026-05-24 21:07:39 -0300

    teste de driver.js

--- apps/front/app/(erros)/sem-permissao/page.tsx
+++ apps/front/app/(erros)/sem-permissao/page.tsx
@@ -3,7 +3,7 @@ import SemAutorizacao from "@/components/erros/semAutorizacao";
 
 export default function SemPermissaoPage(){
     return(
-        <section className="flex flex-1 w-full items-center justify-center">
+        <section className="flex flex-1 w-full items-center justify-center bg-white">
             <SemAutorizacao/>
         </section>
     )
