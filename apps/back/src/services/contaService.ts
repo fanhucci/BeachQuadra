6b16ca2cafa592ca0da4830e4dd707a7b11e7f21
fanhucci <felipeanhucci38@gmail.com>
2026-05-25 05:07:39 -0300

    criação de função de deleção de usuario

--- apps/back/src/services/contaService.ts
+++ apps/back/src/services/contaService.ts
@@ -75,7 +75,7 @@ export default class ContaService{
         const senhaFake = crypto.randomBytes(32).toString('hex');
         const senhaHash = await bcrypt.hash(senhaFake,10)
         console.log(dados, JSON.stringify(dados))
-        const {id_conta} = await this.conta.buscarContaPorPessoa(dados.id_pessoa);
+        const id_conta = await this.conta.buscarContaPorPessoa(dados.id_pessoa);
         console.log(id_conta, typeof id_conta)
         if(!id_conta) throw new AppError('Conta não encotrada', 404);
 
